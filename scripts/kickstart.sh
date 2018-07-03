6c7332fd1eba2622db49bb85ae7b3e62fbbf3773
Geoffrey Gouez <geoffrey@taotesting.com>
2018-07-03 14:27:31 +0200

    add somme timestamps

--- scripts/kickstart.sh
+++ scripts/kickstart.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 #
 
+
+_ts(){
+  date '+%Y-%m-%d %H:%M:%S'
+}
+
+_log(){
+  echo "$(_ts) $@"
+}
+
+_log start kickstart script
+
+
 sudo ln -s /opt/rbenv/bin/rbenv /usr/local/bin/rbenv
 ln -s /opt/rbenv /home/cphp/.rbenv
+
+_log start checkout rbenv
 sudo su -c 'cd /home/cphp/.rbenv/plugins/ruby_build && git checkout master && git pull origin master'
 
 export PATH=/home/cphp/.rbenv/shims:${PATH}
 
-
+_log start rbenv install
 rbenv install 2.4.0
+
+_log start rbenv rehash
 rbenv rehash
-gem install bundle
+
+_log install bundler
+gem install bundler
 
 ruby -v
 
