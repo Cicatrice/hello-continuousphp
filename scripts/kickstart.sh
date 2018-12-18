31511e9b8c9c1f27f0d8785fba304d701b9ed23c
Geoffrey Gouez <geoffrey@taotesting.com>
2018-12-18 17:20:35 +0100

    test yarn install

--- scripts/kickstart.sh
+++ scripts/kickstart.sh
@@ -23,7 +23,9 @@ nvm use 8.14
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo 'deb https://dl.yarnpkg.com/debian/ stable main' | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt-get update && sudo apt-get -y  install apt-transport-https yarn
+sudo apt-get update 
+sudo apt-get -y  install apt-transport-https
+sudo apt-get -y  install yarn
 
 yarn --version
 
