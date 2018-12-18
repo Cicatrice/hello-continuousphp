75cc46bb2d60294ff8d58915a7ab0739c0ab1c3d
Geoffrey Gouez <geoffrey@taotesting.com>
2018-12-18 17:22:06 +0100

    test yarn install

--- scripts/kickstart.sh
+++ scripts/kickstart.sh
@@ -12,6 +12,9 @@ _log(){
 
 _log start kickstart script
 
+sudo apt-get update 
+sudo apt-get -y  install apt-transport-https
+
 . /usr/local/src/nvm/nvm.sh
 sudo rm -f /usr/local/bin/nodejs /usr/local/bin/node
 
@@ -24,7 +27,6 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo 'deb https://dl.yarnpkg.com/debian/ stable main' | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt-get update 
-sudo apt-get -y  install apt-transport-https
 sudo apt-get -y  install yarn
 
 yarn --version
