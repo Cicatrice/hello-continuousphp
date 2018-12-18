6c3d9313f5dfad3020eaef35e090e4cea3f36cda
Geoffrey Gouez <geoffrey@taotesting.com>
2018-12-18 16:43:28 +0100

    test quick&dirty node

--- scripts/kickstart.sh
+++ scripts/kickstart.sh
@@ -13,28 +13,8 @@ _log(){
 _log start kickstart script
 
 
-sudo ln -s /opt/rbenv/bin/rbenv /usr/local/bin/rbenv
-ln -s /opt/rbenv /home/cphp/.rbenv
-
-_log start checkout rbenv
-sudo su -c 'cd /home/cphp/.rbenv/plugins/ruby_build && git checkout master && git pull origin master'
-
-export PATH=/home/cphp/.rbenv/shims:${PATH}
-
-_log start rbenv install
-rbenv install 2.4.0
-_log start rbenv global  
-sudo rbenv global 2.4.0
-
-
-_log start rbenv rehash
-rbenv rehash
-
-_log install bundler
-gem install bundler
-
-_log finish
-ruby -v
+ls -l $(which node)
+ls -l $(which nodejs)
 
 
 
